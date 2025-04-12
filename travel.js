956c66072c481449a30b565898c39904b7bfa9cf
mr0008 <zerolimit086@gmail.com>
2025-04-12 09:13:20 +0530

    Add files via upload

---
+++ travel.js
@@ -0,0 +1,12 @@
+function horton() {
+    alert("Your are going to be navigate Horton Plains page");
+}
+function yala() {
+    alert("Your are going to be navigate Yala page");
+}
+function rever() {
+    alert("Your are going to be navigate Reverston page");
+}
+function sigi() {
+    alert("Your are going to be navigate Sigiriya page");
+}
